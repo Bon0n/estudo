3c50ed9930f30e39d6451e6ab51e180c2865fab0
Andrei Bonon <andrei.bonon@outlook.com>
2022-01-28 16:02:20 -0300

    Http rodando

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-//Config Express - Usado para subir servidor html
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 
 //Config Sequelize
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('sistemadecadastro','root','R0omArmy',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
 
-//Config
-    //Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars')
+// Config
+    // Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    // Conexão com banco de dados MySql
+    const sequelize = new Sequelize('sistemadecadastro','root','R0omArmy',{
+        host: 'localhost',
+        dialect: 'mysql'
+    });
+// Rotas
+    app.get('/cad', function(req,res){
+        res.render('formulario');
+    });
+    app.post('/add', function(req,res){
+        res.send('Formulário recebido');
+    });
 
 app.listen(8081, function(){
     console.log("O Servidor está rodando na porta 8081");
